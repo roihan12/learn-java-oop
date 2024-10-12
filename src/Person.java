c8065bbbff0c0e88d96d6e57e260c5c927a23ecd
Your Name <roihansori12@gmail.com>
2024-10-12 23:24:52 +0700

    learn : constructor and constructor overloading

--- src/Person.java
+++ src/Person.java
@@ -4,6 +4,19 @@ class Person {
 
     final String country = "Indonesia";
 
+
+    Person(String paramName, String paramAddress) {
+        name = paramName;
+        address = paramAddress;
+    }
+
+    Person(String paramName) {
+       this(paramName, null);
+    }
+    Person() {
+        this(null);
+    }
+
     void sayHello(String paramName) {
 
         System.out.println("Hello " + paramName + ", My name is " + name);
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,12 +1,12 @@
 public class PersonApp {
     public static void main(String[] args) {
-        var person1 = new Person();
-        person1.name = "Roihan Sori";
-        person1.address = "Bogor";
+        var person1 = new Person("Roihan", "Jasinga");
+//        person1.name = "Roihan Sori";
+//        person1.address = "Bogor";
         person1.sayHello("Jaffar");
-        Person person2 = new Person();
-        person2.name = "Roihan";
-        person2.address = "Jasinga";
+        Person person2 = new Person("Hadi");
+//        person2.name = "Roihan";
+//        person2.address = "Jasinga";
 
         Person person3;
         person3 = new Person();
