1dda3db322609685b364e1343dc1d690c28854a4
Your Name <roihansori12@gmail.com>
2024-10-13 23:06:46 +0700

    learn: inheritance java

---
+++ src/ManagerApp.java
@@ -0,0 +1,13 @@
+public class ManagerApp {
+    public static void main(String[] args) {
+        var manager = new Manager();
+        manager.name = "Roihan";
+        manager.sayHello("Ahmmad");
+
+        var vp = new VicePresident();
+        vp.name = "Test";
+        vp.sayHello("Test 1");
+
+
+    }
+}
